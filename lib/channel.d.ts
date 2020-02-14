775e66f9d305aa01ee4ce2375558600fa8c139ae
wilfreddenton <dentonw3@gmail.com>
2020-02-14 22:30:01 +0000

    update lib

--- lib/channel.d.ts
+++ lib/channel.d.ts
@@ -1,8 +1,8 @@
 export default class Channel<T> {
     private buffer;
-    private emitter;
-    private zero;
+    private waiters;
     private closed;
+    private zero;
     constructor(zero: T);
     send(item: T): void;
     receive(): Promise<T>;
